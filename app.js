550baf141e0067eb58a723a4f277d3ff337ac807
Kaden Rigsbee <kjrigsbee@gmail.com>
2023-11-30 10:02:10 -0700

    wooo new info

---
+++ app.js
@@ -0,0 +1,9 @@
+console.log('hello there! ~Obi-Wan Kenobi')
+
+function updateThings(){
+    array.forEach(element => {
+        let thingEl = document.getElementByID(element.name)
+        console.log('💀', thingEl)
+        let statsElm = thingElm.querySelector('.stats')
+    });
+}
